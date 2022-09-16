b277ee31506efdd4446b2dcdf949a7bafcbe93c8
Zach R <zarutman+git@gmail.com>
2022-09-15 21:26:10 -0700

    feat: use smartdashboard for target id

--- src/main/java/org/team1540/robot2022/RobotContainer.java
+++ src/main/java/org/team1540/robot2022/RobotContainer.java
@@ -1,17 +1,13 @@
 package org.team1540.robot2022;
 
 import com.ctre.phoenix.motorcontrol.NeutralMode;
-import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import org.photonvision.PhotonCamera;
 import org.photonvision.common.hardware.VisionLEDMode;
 import org.team1540.robot2022.commands.drivetrain.Drivetrain;
 import org.team1540.robot2022.commands.drivetrain.PointToTarget;
 import org.team1540.robot2022.utils.ChickenPhotonCamera;
-import org.team1540.robot2022.utils.NavX;
 
 public class RobotContainer {
     // Hardware
@@ -19,7 +15,6 @@ public class RobotContainer {
     public final ChickenPhotonCamera limelight = new ChickenPhotonCamera("limelight");
 
     public final Drivetrain drivetrain = new Drivetrain(NeutralMode.Brake);
-    private final PointToTarget pointToTargetCommand = new PointToTarget(drivetrain, limelight, 1);
     // Controllers
     public final XboxController driverController = new XboxController(0);
 
@@ -31,13 +26,16 @@ public class RobotContainer {
     }
 
     private void configureButtonBindings() {
-        new Trigger(driverController::getLeftBumper).whileActiveOnce(pointToTargetCommand);
+        new Trigger(driverController::getLeftBumper).whileActiveOnce(
+                new PointToTarget(drivetrain, limelight, (int) SmartDashboard.getNumber("pointToTarget/targetID", 1))
+        );
     }
 
     private void initSmartDashboard() {
         SmartDashboard.setDefaultNumber("pointToTarget/kP", 0.006);
         SmartDashboard.setDefaultNumber("pointToTarget/kI", 0);
         SmartDashboard.setDefaultNumber("pointToTarget/kD", 0.015);
+        SmartDashboard.setDefaultNumber("pointToTarget/targetID", 1);
     }
 
 }
